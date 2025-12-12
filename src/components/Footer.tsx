f4e40498c76e5610a54332268f6d3e5579ef99ec
Abhisek Chatterjee <abhisekc154@gmail.com>
2025-12-13 01:05:15 +0530

    Bold the author's name in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
           <div className="text-center md:text-left mb-6 md:mb-0">
             <p className="text-muted-foreground flex items-center justify-center md:justify-start gap-2">
               © {currentYear} Made 
-              by Abhisek Chatterjee
+              by <b className="text-white">Abhisek Chatterjee</b>
             </p>
           </div>
           
